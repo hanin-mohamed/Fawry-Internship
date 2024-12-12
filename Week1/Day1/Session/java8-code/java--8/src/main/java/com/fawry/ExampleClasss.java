ff5aa572383599a1523f9d585519b653d6d3d997
HoOnn <148750499+Ho0nn@users.noreply.github.com>
2024-12-13 00:50:16 +0200

    Manual Testing

--- Week1/Day1/Session/java8-code/java--8/src/main/java/com/fawry/ExampleClasss.java
+++
@@ -1,9 +0,0 @@
-package com.fawry;
-
-public class ExampleClasss implements ExampleInterface{
-
-    @Override
-    public void exampleMethod(String msg) {
-        System.out.println(msg);
-    }
-}
